155ff99180db62f38899d30f7dc26ae88e63be94
dsywnwlnbmdzs <dsywnwlnbmdzs@gmail.com>
2026-07-22 21:28:53 -0400

    4.0.7

---
+++ javascripts/discourse/routes/trust-level-current-user.js
@@ -0,0 +1,17 @@
+import { service } from "@ember/service";
+import DiscourseRoute from "discourse/routes/discourse";
+
+export default class TrustLevelCurrentUserRoute extends DiscourseRoute {
+  @service currentUser;
+
+  beforeModel() {
+    if (!this.currentUser) {
+      window.location.assign(
+        `/login?redirect=${encodeURIComponent("/u/trust-level")}`
+      );
+      return;
+    }
+
+    this.replaceWith("user.trust-level", this.currentUser.username);
+  }
+}
